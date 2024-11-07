05e543a0eda89cecb2f0c64e35f2c90fc960c061
Nate Wilcox <nejucomo+dev@gmail.com>
2024-11-06 17:12:01 -0800

    Make `nix build` build a package with the binary and the plots.

--- flake.nix
+++ flake.nix
@@ -10,9 +10,9 @@
           pname = "zec-issuance-plots";
           version = (
             let
-              inherit (builtins) readFile fromToml;
-              cargoToml = fromToml (readFile ./Cargo.toml);
-            in 
+              inherit (builtins) readFile fromTOML;
+              cargoToml = fromTOML (readFile ./Cargo.toml);
+            in
               cargoToml.package.version
           );
 
@@ -26,29 +26,73 @@
             mkShell
             writeScript
           ;
-        in {
-          packages.default = pkgs.stdenv.mkDerivation {
-            inherit pname version;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            cmake
+            expat
+            fontconfig.dev
+            freetype
+          ];
+
+          devShellInputs = with pkgs; [
+            cargo
+          ];
+
+          rustPkg = pkgs.rustPlatform.buildRustPackage {
+            inherit pname version nativeBuildInputs buildInputs;
+            src = ./.;
+            # cargoBuildFlags = "-p app";
+
+            # patchPhase = ''
+
+            #   set -x
+
+            #   PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --libs --cflags fontconfig
+
+            #   set +x
+            #   exit 1
+            # '';
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+
+          plotsPname = "${pname}-plots";
+          plotsPkg = pkgs.stdenv.mkDerivation {
+            pname = plotsPname;
+            inherit version;
 
             src = ./.;
-            builder = writeScript "build_${pname}" ''
+
+            builder = writeScript "build_${plotsPname}" ''
               source "$stdenv/setup"
 
-              echo 'FIXME: nix package build is not yet implemented.'
-              exit 1
+              set -ex
+
+              eval "${rustPkg}/bin/zec-issuance-model"
+
+              basedir="$out/share/doc/"
+              mkdir -p "$basedir"
+              mv plots "$basedir/${pname}"
+              set +x
             '';
+          };
+        in {
+          packages.default = pkgs.symlinkJoin {
+            pname = "${pname}-with-plots";
+            inherit version;
+
+            paths = [ rustPkg plotsPkg ];
           };
 
           devShells.default = mkShell {
-            buildInputs = with pkgs; [
-              cargo
-              cmake
-              expat
-              # file
-              fontconfig
-              freetype
-              pkg-config
-            ];
+            inherit buildInputs;
+            nativeBuildInputs = nativeBuildInputs ++ devShellInputs;
           };
         }
       )
